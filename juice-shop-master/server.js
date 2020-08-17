f8faa2950e59d5cde855110ccd6a651f6031e37d
Nikhil Lalkota <nikhil.lalkota@Nikhil.Lalkota>
2020-08-17 14:56:00 -0400

    server.js

--- juice-shop-master/server.js
+++ juice-shop-master/server.js
@@ -21,7 +21,6 @@ const robots = require('express-robots-txt')
 const yaml = require('js-yaml')
 const swaggerUi = require('swagger-ui-express')
 const RateLimit = require('express-rate-limit')
-const swaggerDocument = yaml.load(fs.readFileSync('./swagger.yml', 'utf8'))
 const {
   ensureFileIsPassed,
   handleZipFileUpload,
